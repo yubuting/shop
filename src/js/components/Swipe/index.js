8b9c73f5073d99c9586e3eafc9160ceaa2da3303
xiongtt <xiongtt@tvbozone.com>
2017-04-20 19:37:12 +0800

    @20170420----添加首页轮播

--- src/js/components/Swipe/index.js
+++ src/js/components/Swipe/index.js
@@ -4,7 +4,32 @@ import { Carousel } from 'antd-mobile'
 export default class Swipe extends Component {
   render() {
     const { data, hackHeight, ...rest } = this.props
-    /*如果接收到一张图片就单纯展示，如果是多张，就用Carousel组件展示*/
+
+    let list
+    if(data.length === 1){
+      list = 
+        <a href={data[0].link} className="block">
+          <img src={data[0].thumb} className={'img-responsive'} />
+        </a>
+    } else {
+      list = 
+        <Carousel {...rest}></Carousel>
+    }
+
+    return (<div>
+      {list}
+    </div>)
+
+  }
+}
+
+/*import React , {　Component } from 'react'
+import { Carousel } from 'antd-mobile'
+
+export default class Swipe extends Component {
+  render() {
+    const { data, hackHeight, ...rest } = this.props
+    {/*如果接收到一张图片就单纯展示，如果是多张，就用Carousel组件展示}
     let list
     if (data.length === 1) {
       list =
@@ -37,4 +62,4 @@ export default class Swipe extends Component {
       </div>
     )
   }
-}
+}*/
